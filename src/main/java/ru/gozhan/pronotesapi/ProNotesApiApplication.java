14e66ea0cf2bbce170f4fa763ad3e7f448dba00f
sanchellik <sashagozhan@gmail.com>
2024-10-16 02:53:23 +0300

    init

---
+++ src/main/java/ru/gozhan/pronotesapi/ProNotesApiApplication.java
@@ -0,0 +1,13 @@
+package ru.gozhan.pronotesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProNotesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProNotesApiApplication.class, args);
+    }
+
+}
